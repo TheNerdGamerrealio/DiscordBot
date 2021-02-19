2fd38d3c04adb6a968d014f022471bd0676ef7b0
TheNerdGamerrealio <67924886+TheNerdGamerrealio@users.noreply.github.com>
2021-02-18 19:02:14 -0600

    Add files via upload

---
+++ Discord Bot/Discord Bot/Bot.cs
@@ -0,0 +1,57 @@
+﻿using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text;
+using Newtonsoft.Json;
+using Discord_Bot.Commands;
+
+namespace Discord_Bot
+{
+    class Bot
+    {
+        public DiscordClient Client { get; private set; }
+        public CommandsNextExtension Commands { get; private set; }
+
+        public async Task RunAsync()
+        {
+            var json = string.Empty;
+
+            using (var fs = File.OpenRead("config.json"))
+            using (var sr = new StreamReader(fs, new UTF8Encoding(false)))
+                json = await sr.ReadToEndAsync().ConfigureAwait(false);
+
+            var ConfigJson = JsonConvert.DeserializeObject<ConfigJson>(json);
+
+            var config = new DiscordConfiguration
+            {
+                Token = ConfigJson.Token,
+                TokenType = TokenType.Bot,
+                AutoReconnect = true,
+                MinimumLogLevel = LogLevel.Debug
+            };
+
+            var commandsConfig = new CommandsNextConfiguration
+            {
+                StringPrefixes = new string[] { ConfigJson.Prefix },
+                EnableMentionPrefix = false
+            };
+
+            Client = new DiscordClient(config);
+            Commands = Client.UseCommandsNext(commandsConfig);
+
+            Commands.RegisterCommands<GeneralCommands>();
+
+            Client.Ready += Client_Ready;
+
+            await Client.ConnectAsync();
+            await Task.Delay(-1);
+        }
+
+        private Task Client_Ready(DiscordClient sender, DSharpPlus.EventArgs.ReadyEventArgs e)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ Discord Bot/Discord Bot/Commands/GeneralCommands.cs
@@ -0,0 +1,15 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using System.Threading.Tasks;
+
+namespace Discord_Bot.Commands
+{
+    class GeneralCommands : BaseCommandModule
+    {
+        [Command("helloWorld")]
+        public async Task HelloWorld(CommandContext ctx)
+        {
+            await ctx.Channel.SendMessageAsync("Hello there!");
+        }
+    }
+}
